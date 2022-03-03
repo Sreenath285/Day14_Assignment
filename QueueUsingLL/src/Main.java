8c524554ccb22cbdba7d38fb4921fea181afa535
Sreenath285 <sreenath.285@gmail.com>
2022-03-03 19:23:51 +0530

    dequeue element from the beginning

---
+++ QueueUsingLL/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    /***
+     * main method for manipulation
+     * @param args - default param(not used)
+     */
+    public static void main(String[] args) {
+        /***
+         *PROCEDURE
+         * 1. creating queue
+         * 2. adding elements into queue
+         * 3. printing elements after adding them into queue
+         * 4. dequeue element from the beginning
+         * 5. printing elements after dequeue element from the beginning
+         */
+
+        /***
+         * 1. creating queue
+         */
+        QueueNode queueNode = new QueueNode();
+
+        /***
+         * 2. adding elements into queue
+         */
+        queueNode.enQueue(56);
+        queueNode.enQueue(30);
+        queueNode.enQueue(70);
+
+        /***
+         * 3. printing elements after adding them into queue
+         */
+        System.out.println("Elements in the queue : ");
+        queueNode.printQueue();
+
+        /***
+         * 4. dequeue element from the beginning
+         */
+        queueNode.deQueue();
+
+        /***
+         * 5. printing elements after dequeue element from the beginning
+         */
+        System.out.println("Elements after dequeue: ");
+        queueNode.printQueue();
+    }
+}
---
+++ QueueUsingLL/src/QueueNode.java
@@ -0,0 +1,52 @@
+public class QueueNode {
+
+    MyNode start, end;
+    int count = 0;
+
+    /***
+     * enQueue method is created to add elements into queue
+     * @param data - data is to add in queue
+     */
+    public void enQueue(int data) {
+
+        MyNode newNode = new MyNode(data);
+        if (end == null) {
+            end = start = newNode;
+            count++;
+        }
+        else {
+            end.next = newNode;
+            end = newNode;
+            count++;
+        }
+    }
+
+    /***
+     * printQueue method is created to print queue elements
+     */
+    public void printQueue() {
+
+        MyNode node = start;
+        while (node.next != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println(node.data);
+    }
+
+    /***
+     * deQueue method is created to dequeue element from the beginning
+     */
+    public void deQueue() {
+
+        if (start == null) {
+            System.out.println("Empty queue");
+        }
+
+        start = start.next;
+        if (start == null ) {
+            end = null;
+        }
+        count--;
+    }
+}
